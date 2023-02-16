479fae480a4d267d6a2bdeb81be574197a34eb43
NatKhomich <yalapulya94@mail.ru>
2023-02-16 17:59:13 +0600

    first commit react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
+function hello() {
+    debugger
+    alert('Hello IT-KAMASUTRA')
+}
+//hello();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        //JSX
+        <div>
+            <PageTitle title = {"This is APP component"}/>
+            <PageTitle title = {"My friends"}/>
+            Article 1
+            <Rating value = {3}/>
+            <Accordion/>
+            Article 2
+            <Rating value = {0}/>
+            <Rating value = {1}/>
+            <Rating value = {2}/>
+            <Rating value = {3}/>
+            <Rating value = {4}/>
+            <Rating value = {5}/>
+        </div>
+    );
+}
+
+function PageTitle(props: any) {
+    debugger
+    console.log("PageTitle rendering")
+    return <h1>{ props.title }</h1>
 }
 
 export default App;
